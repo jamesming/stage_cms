9bfac34eb9c7735200ece5321355e84d3ae8c0ee
James Ming <jamesming@gmail.com>
2011-11-09 15:17:22 -0800

    added

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -218,6 +218,7 @@ function upload_photos_success(){
 	);
 	
 	
+	chmod_R('uploads/showpage_photos_items_images/'. $this->input->get('showpage_item_id'), 0666, 0777);
 	chmod('uploads/showpage_photos_items_images/'. $this->input->get('showpage_item_id').'/'. $this->input->get('showpage_photos_item_id').'/fullsize/' . $new_name, 0755);
 	chmod('uploads/showpage_photos_items_images/'. $this->input->get('showpage_item_id').'/'. $this->input->get('showpage_photos_item_id').'/thumb/' . $new_name, 0755);
 		
