247e10c741f103c239c27d97a0a00826f3bf47fd
James Ming <jamesming@gmail.com>
2011-11-25 07:14:35 -0800

    added

--- brightcove.php
+++ brightcove.php
@@ -262,49 +262,39 @@ JSONscriptRequest.prototype.addScriptTag = function () {
 
 <div id="bcPlayer">
 
-  <!-- Start of Brightcove Player -->
+<!-- Start of Brightcove Player -->
 
-  <div style="display:none"> Experience for playing back videos. </div>
+<div style="display:none">
 
+</div>
 
-
-  <!--
-
+<!--
 By use of this code snippet, I agree to the Brightcove Publisher T and C 
-
-found at http://corp.brightcove.com/legal/terms_publisher.cfm. 
-
+found at https://accounts.brightcove.com/en/terms-and-conditions/. 
 -->
 
-  <script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script>
+<script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script>
 
-  <script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/APIModules_all.js"></script>
+<object id="myExperience" class="BrightcoveExperience">
+  <param name="bgcolor" value="#FFFFFF" />
+  <param name="width" value="802" />
+  <param name="height" value="427" />
+  <param name="playerID" value="975126117001" />
+  <param name="playerKey" value="AQ~~,AAAAADEdZAY~,VHcBVNPqDVtFfgMts1b_9xI-OSJcija0" />
+  <param name="isVid" value="true" />
+  <param name="isUI" value="true" />
+  <param name="dynamicStreaming" value="true" />
+  
+</object>
 
-  <object id="myExperience" class="BrightcoveExperience">
+<!-- 
+This script tag will cause the Brightcove Players defined above it to be created as soon
+as the line is read by the browser. If you wish to have the player instantiated only after
+the rest of the HTML is processed and the page load is complete, remove the line.
+-->
+<script type="text/javascript">brightcove.createExperiences();</script>
 
-    <param name="bgcolor" value="#FFFFFF" />
-
-    <param name="width" value="480" />
-
-    <param name="height" value="270" />
-
-    <param name="playerID" value="441686777001" />
-
-
-
-    <param name="publisherID" value="14459790001"/>
-
-    <param name="isVid" value="true" />
-
-    <param name="isUI" value="true" />
-
-    <param name="dynamicStreaming" value="true" />
-
-    <param name="@videoPlayer" value="441742807001" />
-
-  </object>
-
-  <!-- End of Brightcove Player -->
+<!-- End of Brightcove Player -->
 
 </div>
 
