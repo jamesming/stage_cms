3a43cd3b4457ccf271a4f41a1ed1a3d88ad35c84
James Ming <jamesming@gmail.com>
2011-11-25 08:26:36 -0800

    added

--- brightcove.php
+++ brightcove.php
@@ -122,7 +122,7 @@ JSONscriptRequest.prototype.addScriptTag = function () {
 
 	var req = "http://api.brightcove.com/services/library?";
 
-	req += "command=find_all_videos&token=" + encodeURIComponent(token);  // tokens need to be URL-encoded
+	req += "command=find_playlist_by_id&playlist_id=928432058001&playlist_fields=id,name,shortDescription,thumbnailURL,length&token=" + encodeURIComponent(token);  // tokens need to be URL-encoded
 
 	req += "&fields=id,name,shortDescription,thumbnailURL,length&callback=response";
 
