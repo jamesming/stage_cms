707866b275b98942193b524df908fc998c89a9ec
James Ming <jamesming@gmail.com>
2011-11-27 08:47:55 -0800

    added

--- brightcove.php
+++ brightcove.php
@@ -17,116 +17,7 @@ bounceOut	 Bounces off end.
 bounceBoth	 Bounces off start and end.
 -->
 <?php     
-	 function object_to_array($data){
-	  if(is_array($data) || is_object($data)){
-	    $result = array(); 
-	    foreach($data as $key => $value)
-	    { 
-	      $result[$key] = $this->object_to_array($value); 
-	    }
-	    return $result;
-	  }
-	  return $data;
-	}
-
-  function jsonDecode ($json) { 
-      $json = str_replace(array("\\\\", "\\\""), array("&#92;", "&#34;"), $json); 
-      $parts = preg_split("@(\"[^\"]*\")|([\[\]\{\},:])|\s@is", $json, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE); 
-      foreach ($parts as $index => $part) 
-      { 
-          if (strlen($part) == 1) 
-          { 
-              switch ($part) 
-              { 
-                  case "[": 
-                  case "{": 
-                      $parts[$index] = "array("; 
-                      break; 
-                  case "]": 
-                  case "}": 
-                      $parts[$index] = ")"; 
-                      break; 
-                  case ":": 
-                    $parts[$index] = "=>"; 
-                    break;    
-                  case ",": 
-                    break; 
-                  default: 
-                      return null; 
-              } 
-          } 
-          else 
-          { 
-              if ((substr($part, 0, 1) != "\"") || (substr($part, -1, 1) != "\"")) 
-              { 
-                  return null; 
-              } 
-          } 
-      } 
-      $json = str_replace(array("&#92;", "&#34;", "$"), array("\\\\", "\\\"", "\\$"), implode("", $parts)); 
-      return eval("return $json;"); 
-  } 
-	
-	
-	$playlists = array(
-		0 => array(
-					'playlist_name' => '101 Extras',
-					'id' => '48759451001'
-					),
-		1 => array(
-					'playlist_name' => 'Comedy',
-					'id' => '58518976001'
-					),
-		2 => array(
-					'playlist_name' => 'Osmin Webisodes',
-					'id' => '1067741571001'
-					),
-		3 => array(
-					'playlist_name' => 'Extreme Extras',
-					'id' => '58491372001'
-					),	
-		5 => array(
-					'playlist_name' => 'Model Latina Miami',
-					'id' => '35098540001'
-					),
-		5 => array(
-					'playlist_name' => 'Mission Menu',
-					'id' => '1163757251001'
-					),							
-									
-	);
-	
-	
-	foreach( $playlists  as  $playlist){
-			$json_url = 'http://api.brightcove.com/services/library?command=find_playlist_by_id&media_delivery=http&token=mfu5Nh2a27pJx7LrgZbLx363WrLDHUmtJ5BXY0GkYK4.&playlist_id='.$playlist['id'];
-			$ch = curl_init( $json_url );
-				$options = array(
-				CURLOPT_RETURNTRANSFER => true,
-				CURLOPT_HTTPHEADER => array('Content-type: application/json')
-			);
-			
-			curl_setopt_array( $ch, $options );
-			$result = curl_exec($ch);
-			$video['clips']  = json_decode($result)->videos;	
-			$video['playlist_name'] = $playlist['playlist_name'];
-			$videos[]= 	$video;
-	}
-
-
-	
-	
-//	$playlist_id ='58518976001';
-//	$json_url = 'http://api.brightcove.com/services/library?command=find_playlist_by_id&media_delivery=http&token=mfu5Nh2a27pJx7LrgZbLx363WrLDHUmtJ5BXY0GkYK4.&playlist_id='.$playlist_id;
-//	$ch = curl_init( $json_url );
-//		$options = array(
-//		CURLOPT_RETURNTRANSFER => true,
-//		CURLOPT_HTTPHEADER => array('Content-type: application/json')
-//	);
-//	
-//	curl_setopt_array( $ch, $options );
-//	$result = curl_exec($ch);
-//	$videos2 = json_decode($result)->videos;
-
+include('brightcove_prepare.php');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
@@ -134,6 +25,90 @@ bounceBoth	 Bounces off start and end.
 
 <head>
 
+
+<link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen, projection">
+<link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="print">
+<!--[if IE]><link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen, projection"><![endif]-->
+
+<!-- Import fancy-type plugin for the sample page. -->
+<link rel="stylesheet" href="css/blueprint/plugins/fancy-type/screen.css" type="text/css" media="screen, projection">
+
+<link rel="stylesheet" href="css/grid.css?<?php echo rand()    ?>"  type="text/css" >
+<link rel="stylesheet" href="css/custom.css?<?php echo rand()    ?>"  type="text/css" >
+<style>
+.container{
+	width:978px;
+}
+.video_title_bar{
+    background-image: url("assets/images/VIDEO-TITLE_bar.png");
+    background-position: center center;
+    background-repeat: no-repeat;
+    margin-top: 10px;
+    font-family: sans-serif;
+    font-weight: bold;
+    color: white;
+    font-size: 13px;
+    padding-left: 16px;
+    height: 41px;
+    padding-top: 19px;
+}
+
+.video_title_bar .title-text{
+	float:left;	
+	width: 900px;
+}
+.video_title_bar .scroll-button{
+	height:22px;
+	float:left;
+	width:18px;
+	cursor:pointer;
+}
+.video_title_bar .scroll-button.left{
+	margin-right: 9px;
+}
+.video_title_bar .scroll-button.right{
+}
+.video_scroller li{
+cursor:pointer;	
+margin-right:20px;
+margin-left:10px;
+border:0px solid gray;
+width:300px;
+height:80px;
+}
+
+
+.video_scroller li .img-div{
+width:150px;
+float:left;
+background:yellow;
+}
+
+.video_scroller li .img-div img{
+width:150px;
+}
+
+.video_scroller li .text-div{
+width:135px;
+font-size:10px;
+font-weight:bold;
+float:left;
+}
+
+.video_scroller li  .text-div .video-name{
+padding-left:11px;
+height:53px;
+}
+
+.video_scroller li  .text-div .watch-now{
+background-image: url("assets/images/VIDEO-watch-now-GRAY.png");
+background-position: center center;
+background-repeat: no-repeat;
+height:33px;
+width: 150px;
+}
+
+</style>
 
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.js"></script>
@@ -704,68 +679,10 @@ function handler(event) {
 <body onload="initCall()">
 
 
-<!-- Start of Brightcove Player -->
-
-<div style="display:none">
-
-</div>
-
-<!--
-By use of this code snippet, I agree to the Brightcove Publisher T and C 
-found at https://accounts.brightcove.com/en/terms-and-conditions/. 
--->
-
-<script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script>
-  <script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/APIModules_all.js"></script>
-<object id="myExperience" class="BrightcoveExperience">
-  <param name="bgcolor" value="#FFFFFF" />
-  <param name="width" value="802" />
-  <param name="height" value="427" />
-  <param name="playerID" value="975126117001" />
-  <param name="playerKey" value="AQ~~,AAAAADEdZAY~,VHcBVNPqDVtFfgMts1b_9xI-OSJcija0" />
-  <param name="isVid" value="true" />
-  <param name="isUI" value="true" />
-  <param name="dynamicStreaming" value="true" />
-  
-</object>
-
-<!-- 
-This script tag will cause the Brightcove Players defined above it to be created as soon
-as the line is read by the browser. If you wish to have the player instantiated only after
-the rest of the HTML is processed and the page load is complete, remove the line.
--->
-<script type="text/javascript">brightcove.createExperiences();</script>
-
-<!-- End of Brightcove Player -->
 
 
-<style>
 
-.video_scroller li{
-cursor:pointer;	
-margin-right:10px;
-border:1px solid gray;
-width:256px;
-height:80px;
-}
 
-.video_scroller li .img{
-width:110px;
-height:80px;
-}
-
-.video_scroller li .img-div{
-width:120px;
-}
-
-.video_scroller li .video-name{
-padding-left:5px;
-width:80px;
-font-size:10px;
-font-weight:bold;
-}
-
-</style>
 
 <script type="text/javascript" language="Javascript">
 	$(document).ready(function() { 
@@ -787,24 +704,44 @@ font-weight:bold;
 				    
 	});
 </script>
+
+<div  class=' container' >
+	
+<?php     
+include('brightcove_player.php');
+?>
+	
+	
 		<?php for( $i = 0; $i < count($videos) ; $i++){ ?>
 		
 				<div  class='  playlist<?php echo  $i   ?>_container' >
-					<?php echo  $videos[$i]['playlist_name']    ?>&nbsp;&nbsp;
-					<button class="prev<?php echo  $i   ?>"><<</button>
-					<button class="next<?php echo  $i   ?>">>></button>
+					
+					<div  class='video_title_bar ' >
+						<div  class='title-text ' >
+							<?php echo  $videos[$i]['playlist_name']    ?>
+						</div>
+						<div class="scroll-button left prev<?php echo  $i   ?>">&nbsp;</div>
+						<div class="scroll-button right next<?php echo  $i   ?>">&nbsp;</div>	
+											
+					</div>
+
 					        
 					<div class="video_scroller playlist<?php echo  $i   ?>">
 					    <ul>
 					    	<?php foreach($videos[$i]['clips'] as  $video ){?>
 									<li onClick='playTitleFromList(<?php echo $video->id    ?>)'>
-										<div   style='clear:both'  >
-											<div   class='img-div '  style='float:left'>
+										<div>
+											<div   class='img-div ' >
 												<img   src="<?php echo $video->thumbnailURL    ?>" alt="<?php  echo $video->name   ?>" >
 											</div>
-											<div  class='video-name '   style='float:left'>
-												<?php  echo $video->name   ?>
-											</div>											
+											<div  class='text-div ' >
+												<div  class='video-name ' >
+													<?php  echo $video->name   ?>
+												</div>
+												<div  class='watch-now ' >&nbsp;											
+												</div>
+											</div>
+										
 										</div>
 
 									</li>
@@ -814,6 +751,9 @@ font-weight:bold;
 				</div>
 
 		<?php } ?>
+
+</div>
+
 
 
 
