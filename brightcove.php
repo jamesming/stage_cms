16501ec2fbc99cd0cef473dec41ff0b51a66e05c
James Ming <jamesming@gmail.com>
2011-11-27 15:10:55 -0800

    added

--- brightcove.php
+++ brightcove.php
@@ -18,10 +18,144 @@ include('brightcove_head.php');
 <div  class=' container' >
 	
 <?php     
-	include('brightcove_player.php');
+//	include('brightcove_player.php');
 ?>
 	
-<?php if( 1==1){?>
+	
+<style>
+.playlist-title{
+	float:left;
+	border:1px solid gray;
+	width:100px;
+	height:20px;
+	margin-right:10px;
+	cursor:pointer;
+}
+.episodes-videos{
+	border:1px solid gray; 
+	width:320px; 
+	height:200px; 
+	overflow-y:scroll;
+	background:black;
+	clear:both;
+}
+.clips-videos{
+	display:none;
+}
+.episodes-videos li{
+    border: 0px solid white;
+    clear: both;
+    color: white;
+    height: 78px;
+    margin-bottom: 12px;    
+}
+.episodes-videos .img-div{
+	float:left;	
+	width:150px;
+}
+		.episodes-videos .img-div img{
+		width:150px;
+		height:85px;	
+		}
+.episodes-videos .text-div{
+	float: left;
+	width: 120px;
+	padding-top:4px;
+}	
+
+.episodes-videos .text-div .video-name{
+	padding-left:11px;
+	height:54px;
+}
+.episodes-videos .text-div .watch-now{
+	background-image: url("assets/images/watch_now.png");
+	background-position: center center;
+	background-repeat: no-repeat;
+	height:33px;
+	width: 150px;
+}	
+</style>
+
+<script type="text/javascript" language="Javascript">
+
+	$(document).ready(function() { 
+		
+		episodes_global = $('.episodes-videos').html();
+		clips_global = $('.clips-videos').html();		
+		
+		$('#clips-title').click(function(event) {
+			$('.episodes-videos').html(clips_global);
+		});	
+		$('#episodes-title').click(function(event) {
+			$('.episodes-videos').html(episodes_global);
+		});	
+		
+	});
+</script>
+	
+		<?php for( $i = 0; $i <= 1; $i++){ ?>	
+		
+			<div  class=' playlist_name' >
+				<div   id='episodes-title'  class='playlist-title '  >
+					<?php echo  $videos[0]['playlist_name']    ?>
+				</div>
+				<div   id='clips-title'   class='playlist-title '>
+					<?php echo  $videos[1]['playlist_name']    ?>
+				</div>
+				
+			</div>
+			<div   class='episodes-videos'  >
+					    <ul>
+					    	<?php foreach($videos[$i]['clips'] as  $video ){?>
+					    	
+									<li onClick='playTitleFromList(<?php echo $video->id    ?>)'>
+
+											<div   class='img-div ' >
+												<img   src="<?php echo $video->thumbnailURL    ?>" alt="<?php  echo $video->name   ?>" >
+											</div>
+											<div  class='text-div ' >
+												<div  class='video-name ' >
+													<?php  echo $video->name   ?>
+												</div>
+												<div  class='watch-now '   style='	'  >&nbsp;											
+												</div>
+											</div>
+
+									</li>
+									
+					    	<?php } ?>
+					    </ul>
+
+		<?php } ?>
+		
+		
+		<?php for( $i = 1; $i <= 2; $i++){ ?>	
+			<div   class='clips-videos'  >
+			    <ul>
+		    	<?php foreach($videos[$i]['clips'] as  $video ){?>
+		    	
+						<li onClick='playTitleFromList(<?php echo $video->id    ?>)'>
+
+								<div   class='img-div ' >
+									<img   src="<?php echo $video->thumbnailURL    ?>" alt="<?php  echo $video->name   ?>" >
+								</div>
+								<div  class='text-div ' >
+									<div  class='video-name ' >
+										<?php  echo $video->name   ?>
+									</div>
+									<div  class='watch-now '   style='	'  >&nbsp;											
+									</div>
+								</div>
+
+						</li>
+						
+		    	<?php } ?>
+					</ul>
+			</div>					
+		<?php } ?>
+	
+	
+<?php if( 1==2){?>
 
 		<?php for( $i = 0; $i < $first_set_of_playlist_scroll_left_right -1; $i++){ ?>
 		
@@ -66,6 +200,7 @@ include('brightcove_head.php');
 <?php } ?>
 
 
+<?php if( 1== 2){?>
 
 		<div  class='red-area-container ' >
 			
@@ -108,6 +243,9 @@ include('brightcove_head.php');
 			<?php } ?>			
 			
 		</div>
+
+<?php } ?>
+
 
 		
 
